b1404bc7460c689df72544de312ac5c782f58289
nonetonmix <noneagueli@gmail.com>
2025-03-15 18:07:22 +0100

    error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
  // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
 
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nonetonmix.github.io",
-  integrations: [mdx(), sitemap(), react(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind()],
 
   markdown: {
     shikiConfig: {
