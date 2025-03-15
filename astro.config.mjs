e6a6f9abcf10872baef906a8025d5f7be50107f6
nonetonmix <noneagueli@gmail.com>
2025-03-15 18:01:42 +0100

    add deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-// @ts-check
+ // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
@@ -6,9 +6,7 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
-import { transformerNotationHighlight } from "@shikijs/transformers";
 
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
